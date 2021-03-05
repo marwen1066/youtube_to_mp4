e146d947903df37dc54f55f98b62f50ec0a673e0
marwen1066 <79971078+marwen1066@users.noreply.github.com>
2021-03-05 16:48:41 +0100

    Update one1.py

--- one1.py
+++ one1.py
@@ -2,4 +2,4 @@ from pytube import YouTube
 Link = input("write video link: ")
 video = YouTube(Link)
 video.streams.get_highest_resolution().download(output_path="/c:")
-print(video)
+print("download complete" +video.title+ ".mp4")
